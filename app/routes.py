d6b762cf6087d90acc2d3291f9fe51d50896a953
joeykphan <joeykphan@gmail.com>
2023-04-04 12:48:15 -0400

    :building_construction: starting templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "yo world!"
+    user = {'username': 'Joey'}
+    posts = [
+        {
+            'author' : {'username': 'Joey'},
+            'title' : 'CSP',
+            'body' : 'This card is great!'
+        },
+        {
+            'author' : {'username': 'Joey'},
+            'title' : 'CFU',
+            'body' : 'This card is versatile!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
