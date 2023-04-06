c2d741bdc356ed349a5bf119ef45ae6336ce7247
joeykphan <joeykphan@gmail.com>
2023-04-06 19:27:57 -0400

    :construction: adding login page and format

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,24 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Joey'}
+    user = {"username": "Joey"}
     posts = [
+        {"author": {"username": "Joey"}, "title": "CSP", "body": "This card is great!"},
         {
-            'author' : {'username': 'Joey'},
-            'title' : 'CSP',
-            'body' : 'This card is great!'
+            "author": {"username": "Joey"},
+            "title": "CFU",
+            "body": "This card is versatile!",
         },
-        {
-            'author' : {'username': 'Joey'},
-            'title' : 'CFU',
-            'body' : 'This card is versatile!'
-        }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
