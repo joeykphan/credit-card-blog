5567e394a92f5ea2a5548d0ad9b869df7399cc82
joeykphan <joeykphan@gmail.com>
2023-04-07 17:32:50 -0400

    :sparkles: added basic login page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import flash, render_template, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -8,7 +8,11 @@ from app.forms import LoginForm
 def index():
     user = {"username": "Joey"}
     posts = [
-        {"author": {"username": "Joey"}, "title": "CSP", "body": "This card is great!"},
+        {
+            "author": {"username": "Joey"},
+            "title": "CSP",
+            "body": "This card is great to use!",
+        },
         {
             "author": {"username": "Joey"},
             "title": "CFU",
@@ -18,7 +22,12 @@ def index():
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
