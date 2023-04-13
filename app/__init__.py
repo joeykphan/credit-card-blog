8319a0f82900c185f9a22a99b0e55fa0b948554a
joeykphan <joeykphan@gmail.com>
2023-04-13 17:32:10 -0400

    :sparkles: add user login and logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ import sqlalchemy
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"  # type: ignore
 
 from app import routes, models
