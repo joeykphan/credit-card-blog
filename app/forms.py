dd6f10b2a887a1a1c594d33f877b45b803fb677b
joeykphan <joeykphan@gmail.com>
2023-04-14 16:43:46 -0400

    :rotating_light: :label: add some type annotations and :art: format

--- app/forms.py
+++ app/forms.py
@@ -17,16 +17,17 @@ class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField(
-        "Repeat Password", validators=[DataRequired(), EqualTo("password")],
+        "Repeat Password",
+        validators=[DataRequired(), EqualTo("password")],
     )
     submit = SubmitField("Register")
 
-    def validate_username(self, username):
+    def validate_username(self, username: StringField) -> None:
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError("Please use a different username.")
 
-    def validate_email(self, email):
+    def validate_email(self, email: StringField) -> None:
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Please use a different email address.")
--- app/models.py
+++ app/models.py
@@ -16,10 +16,10 @@ class User(UserMixin, db.Model):
     def __repr__(self) -> str:
         return f"<User {self.username}>"
 
-    def set_password(self, password):
+    def set_password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
 
 
