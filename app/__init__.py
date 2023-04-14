899b53608ac0a5ab41fe17c87786c131c6b17cfd
joeykphan <joeykphan@gmail.com>
2023-04-14 16:29:46 -0400

    :art: ruff auto fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from config import Config
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,4 @@ login = LoginManager(app)
 login.login_view = "login"  # type: ignore
 
 # bottom import is a workaround to circular imports
-from app import routes, models
+from app import models, routes
