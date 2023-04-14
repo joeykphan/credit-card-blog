4cff631ff37c023fcebf65807416f84253f47aa2
joeykphan <joeykphan@gmail.com>
2023-04-14 16:44:28 -0400

    :rotating_light: simplify logic as empty string is falsey

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ def login():
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
+        if not next_page or url_parse(next_page).netloc:
             next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
