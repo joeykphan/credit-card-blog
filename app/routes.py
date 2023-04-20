a594cfcc5163371d65d2148a42fc8294d0847281
joeykphan <joeykphan@gmail.com>
2023-04-19 22:56:12 -0400

    :sparkles: add new user attributes to user profile view

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
@@ -68,3 +69,21 @@ def register():
         flash("Congratulations, you are now a registered user!")
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "test post 1"},
+        {"author": user, "body": "test post 2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:  # type: ignore
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
