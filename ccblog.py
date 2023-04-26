0a0878f96524f665478998b66fd3a2aa36ae0e19
joeykphan <joeykphan@gmail.com>
2023-04-25 21:18:57 -0400

    :memo: add ruff documentation

--- ccblog.py
+++ ccblog.py
@@ -1,7 +1,16 @@
+""""Definition for Flask application and database instance."""
+from typing import Any
+
 from app import app, db
 from app.models import Post, User
 
 
 @app.shell_context_processor
-def make_shell_context():
+def make_shell_context() -> dict[str, Any | None]:
+    """Generate shell context for `flask shell`.
+
+    Returns
+    -------
+    Dictionary of object instances to inject into shell session.
+    """
     return {"db": db, "User": User, "Post": Post}
